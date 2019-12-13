7db63e65af5a3eb5d4530b443412b81a46fe5d4d
Felipe Silva <felipe.florianopolis@gmail.com>
2019-12-12 23:01:45 -0300

    Display users in table and modal window for creating new user

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,11 +1,75 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
+    <div class="row mt-5">
+      <div class="col-12">
         <div class="card">
-          <div class="card-header">Example Component</div>
+          <div class="card-header">
+            <h3 class="card-title">Usuários</h3>
 
-          <div class="card-body">Users</div>
+            <div class="card-tools">
+              <button
+                type="button"
+                class="btn btn-block btn-primary btn-sm"
+                data-toggle="modal"
+                data-target="#newUserModal"
+              >
+                Adicionar usuário
+                <i class="fa fa-user-plus"></i>
+              </button>
+            </div>
+          </div>
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Email</th>
+                  <th>Tipo</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>183</td>
+                  <td>John Doe</td>
+                  <td>11-7-2014</td>
+                  <td>
+                    <span class="tag tag-success">Approved</span>
+                  </td>
+                  <td>
+                    <a href="#" class="btn btn-outline-primary btn-sm">
+                      <i class="fa fa-edit"></i>
+                    </a>
+                    <a href="#" class="btn btn-outline-danger btn-sm">
+                      <i class="fa fa-trash"></i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="newUserModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Adicionar novo usuário</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-success">Salvar</button>
+          </div>
         </div>
       </div>
     </div>
